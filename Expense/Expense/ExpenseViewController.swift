a7c746d85f4ec41754976f156311dc7122e1afc4
Kooper Young <61201332+Kupawaki@users.noreply.github.com>
2022-04-28 10:52:02 -0500

    BOOM Big Commit

--- Expense/Expense/ExpenseViewController.swift
+++ Expense/Expense/ExpenseViewController.swift
@@ -6,24 +6,48 @@
 //
 
 import UIKit
+import Firebase
 
-class ExpenseViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ExpenseViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
+{
+    @IBOutlet weak var expenseTV: UITableView!
+    
+    var indivExpenses : [String] = []
+    var expense = NSDictionary()
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        expenseTV.delegate = self
+        expenseTV.dataSource = self
+        
+        fetchData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return indivExpenses.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = expenseTV.dequeueReusableCell(withIdentifier: "reusableCell", for: indexPath)
+        
+        cell.textLabel?.text = indivExpenses[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    func fetchData()
+    {
+        let databaseRef = Database.database().reference()
+        
+        databaseRef.child("expense").observeSingleEvent(of: .value){ [self]snapshot in
+            let expense = snapshot.value as! NSDictionary
+            self.indivExpenses = expense.allKeys as! [String]
+            self.expenseTV.reloadData()
+        }
+    }
 }
--- Expense/Expense/ViewController.swift
+++ Expense/Expense/ViewController.swift
@@ -6,63 +6,19 @@
 //
 
 import UIKit
+import Firebase
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
+class ViewController: UIViewController
 {
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-    {
-        
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
-    {
-        <#code#>
-    }
-    
-    
-    @IBOutlet weak var mainTV: UITableView!
-    var productsArray = [AppleProduct]()
+
+    var databaseRef: DatabaseReference!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        
-        let product1 = AppleProduct("IPhoneX", "IPhone")
-        let product2 = AppleProduct("IPhone XIV Super Mega Awesome", "IPhone")
-        let product3 = AppleProduct("MacBook I", "MacBook")
-        
-        productsArray.append(product1)
-        productsArray.append(product2)
-        productsArray.append(product3)
-        
-        mainTV.delegate = self
-        mainTV.dataSource = self
-    }
-    
-
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
-    {
-        let transition = segue.identifier
-        if transition == "segueOne"
-        {
-            let destination = segue.destination as! SecondViewController
-        }
     }
 }
 
-class AppleProduct
-{
-    //Class attributes (both are optional type String)
-    public var productName:String?
-    public var productCategory:String?
-    
-    //Constructor
-    init(_ name:String?, _ category:String?)
-    {
-        self.productName = name
-        self.productCategory = category
-    }
-}
+
 
