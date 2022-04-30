d57da8ac82ed8345e9b480704b2e83c61f6d1cee
Kooper Young <61201332+Kupawaki@users.noreply.github.com>
2022-04-29 22:02:36 -0500

    Dynamic adding of income values works now

--- Expense/Expense/IncomeViewController.swift
+++ Expense/Expense/IncomeViewController.swift
@@ -4,16 +4,20 @@
 //
 //  Created by student on 4/5/22.
 //
+
 import UIKit
 import Firebase
 
 class IncomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
     @IBOutlet weak var incomeTV: UITableView!
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var valueTF: UITextField!
     
     let ref = Database.database().reference()
-    var indivIncomes : [String] = []
-    var income = NSDictionary()
+    var incomeKeys:[String] = []
+    var incomeValues:[String] = []
+    var incomeSnapshot = NSDictionary()
     
     override func viewDidLoad()
     {
@@ -25,14 +29,14 @@ class IncomeViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return indivIncomes.count
+        return incomeKeys.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
         let cell = incomeTV.dequeueReusableCell(withIdentifier: "reusableCell", for: indexPath)
         
-        cell.textLabel?.text = indivIncomes[indexPath.row]
+        cell.textLabel?.text = incomeKeys[indexPath.row] + ", " + String(incomeValues[indexPath.row])
         
         return cell
     }
@@ -40,10 +44,23 @@ class IncomeViewController: UIViewController, UITableViewDelegate, UITableViewDa
     func fetchData()
     {
         ref.child("income").observeSingleEvent(of: .value)
-        { [self]snapshot in
+        { [self] snapshot in
             let income = snapshot.value as! NSDictionary
-            self.indivIncomes = income.allKeys as! [String]
+            self.incomeKeys = income.allKeys as! [String]
+            self.incomeValues = income.allValues as! [String]
             self.incomeTV.reloadData()
         }
+    }
+    
+    @IBAction func addValueBTNOnClick(_ sender: Any)
+    {
+        if(nameTF.text!.isEmpty || valueTF.text!.isEmpty)
+        {
+            print("Both value and name cannot be empty")
+            return
+        }
+        
+        self.ref.child("income").child(nameTF.text!).setValue(valueTF.text!)
+        fetchData()
     }
 }
