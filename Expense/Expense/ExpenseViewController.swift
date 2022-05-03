ba9aa19dc98453cfdb1a59d7777fd06f03b53db2
Kooper Young <61201332+Kupawaki@users.noreply.github.com>
2022-05-03 11:21:48 -0500

    All but graphs are done

--- Expense/Expense/ExpenseViewController.swift
+++ Expense/Expense/ExpenseViewController.swift
@@ -9,31 +9,34 @@ import Firebase
 
 class ExpenseViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var valueTF: UITextField!
     @IBOutlet weak var expenseTV: UITableView!
     
     let ref = Database.database().reference()
-    var indivExpenses : [String] = []
+    var expenseKeys:[String] = []
+    var expenseValues:[String] = []
     var expense = NSDictionary()
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
         fetchData()
-
         expenseTV.delegate = self
         expenseTV.dataSource = self
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return indivExpenses.count
+        return expenseKeys.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
         let cell = expenseTV.dequeueReusableCell(withIdentifier: "reusableCell", for: indexPath)
         
-        cell.textLabel?.text = indivExpenses[indexPath.row]
+        cell.textLabel?.text = expenseKeys[indexPath.row] + ", " + String(expenseValues[indexPath.row])
         
         return cell
     }
@@ -41,10 +44,26 @@ class ExpenseViewController: UIViewController, UITableViewDelegate, UITableViewD
     func fetchData()
     {
         ref.child("expense").observeSingleEvent(of: .value)
-        { [self]snapshot in
+        { [self] snapshot in
             let expense = snapshot.value as! NSDictionary
-            self.indivExpenses = expense.allKeys as! [String]
+            self.expenseKeys = expense.allKeys as! [String]
+            self.expenseValues = expense.allValues as! [String]
             self.expenseTV.reloadData()
         }
     }
+    
+    
+    @IBAction func addExpenseBTNOnClick(_ sender: Any)
+    {
+        if(nameTF.text!.isEmpty || valueTF.text!.isEmpty)
+        {
+            print("Both value and name cannot be empty")
+            return
+        }
+        
+        self.ref.child("expense").child(nameTF.text!).setValue(valueTF.text!)
+        fetchData()
+    }
+    
+    
 }
